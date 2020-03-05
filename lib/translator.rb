994ffff336c24b2ead655de1cef603066417a161
Alexander Silver <asilver7493@gmail.com>
2020-03-05 20:18:39 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,6 +8,8 @@ def load_library(file_path)
    
  }
  emoticons.each do |key, value|
+   # :get_meaning points to english definitions
+   # :get_emoticon points to Japanese emoticon
    newHash[:get_meaning][value[1]] = key
    newHash[:get_emoticon][value[0]] = value[1]
  end
@@ -17,8 +19,12 @@ end
 
 def get_japanese_emoticon(yaml, str)
   emoticons = load_library(yaml)
+  emote = emoticons[:get_emoticon][str]
+  emote
 end
 
 def get_english_meaning(yaml,str )
  emoticons = load_library(yaml)
+ emote = emoticons[:get_meaning][str]
+ emote
 end
